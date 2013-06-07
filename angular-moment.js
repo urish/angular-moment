765bf0634713bf6848ffc0965ee5c4539e2a7980
Uri Shaked <uri@salsa4fun.co.il>
2013-06-08 01:25:41 +0300

    Added support for passing date as milliseconds since the epoch, version number 0.1.2

--- angular-moment.js
+++ angular-moment.js
@@ -1,11 +1,13 @@
-// angular-moment.js / v0.1.0 / (c) 2013 Uri Shaked / Apache License
+// angular-moment.js / v0.1.2 / (c) 2013 Uri Shaked / Apache License
 
 angular.module('angularMoment', [])
 	.directive('amTimeAgo', function ($timeout) {
+		'use strict';
+
 		return function (scope, element, attr) {
 			function updateTime(momentInstance) {
 				element.text(momentInstance.fromNow());
-				var howOld = moment().diff(momentInstance, "minute");
+				var howOld = moment().diff(momentInstance, 'minute');
 				var secondsUntilUpdate = 3600;
 				if (howOld < 1) {
 					secondsUntilUpdate = 1;
@@ -21,17 +23,19 @@ angular.module('angularMoment', [])
 			}
 
 			scope.$watch(attr.amTimeAgo, function (value) {
-				// Using array to build a Date object no matter of the browser (iPhone fix)
-				// solution taken from: http://stackoverflow.com/questions/5324178/javascript-date-parsing-on-iphone
-				
-				var arr = value.toString().split(/[- :]/),
-				    date = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);
-
-				if (!(value instanceof Date)) {
-					value = new Date(date);
+				if (angular.isNumber(value)) {
+					// Milliseconds since the epoch
+					value = new Date(value);
 				}
+				else if (angular.isString(value)) {
+					// Using array to build a Date object no matter of the browser (iPhone fix)
+					// solution taken from: http://stackoverflow.com/questions/5324178/javascript-date-parsing-on-iphone
+					var arr = value.toString().split(/[- :]/);
+					value = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);
+				}
+				// else assume the given value is already a date
 
 				updateTime(moment(value));
 			});
-		}
+		};
 	});
