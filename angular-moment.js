546a486d0e11f71420074bdc90c4edcc82c1b11a
Uri Shaked <uri@salsa4fun.co.il>
2013-07-08 12:14:10 +0300

    Added unit-tests; resolved issue #5

--- angular-moment.js
+++ angular-moment.js
@@ -1,15 +1,15 @@
 // angular-moment.js / v0.1.3 / (c) 2013 Uri Shaked / MIT Licence
 
 angular.module('angularMoment', [])
-	.directive('amTimeAgo', function ($timeout) {
+	.directive('amTimeAgo', function ($window, $timeout) {
 		'use strict';
 
 		return function (scope, element, attr) {
 			var activeTimeout = null;
-		
+
 			function updateTime(momentInstance) {
 				element.text(momentInstance.fromNow());
-				var howOld = moment().diff(momentInstance, 'minute');
+				var howOld = $window.moment().diff(momentInstance, 'minute');
 				var secondsUntilUpdate = 3600;
 				if (howOld < 1) {
 					secondsUntilUpdate = 1;
@@ -29,19 +29,13 @@ angular.module('angularMoment', [])
 					// Milliseconds since the epoch
 					value = new Date(value);
 				}
-				else if (angular.isString(value)) {
-					// Using array to build a Date object no matter of the browser (iPhone fix)
-					// solution taken from: http://stackoverflow.com/questions/5324178/javascript-date-parsing-on-iphone
-					var arr = value.toString().split(/[- :]/);
-					value = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);
-				}
 				// else assume the given value is already a date
 
 				if (activeTimeout) {
 					$timeout.cancel(activeTimeout);
 					activeTimeout = null;
 				}
-				updateTime(moment(value));
+				updateTime($window.moment(value));
 			});
 		};
 	});
--- tests.js
+++ tests.js
@@ -5,4 +5,84 @@
 'use strict';
 
 describe('Directive: am-time-ago', function () {
+	var $rootScope, $compile, $timeout;
+
+	beforeEach(module('angularMoment'));
+
+	beforeEach(inject(function ($injector) {
+		$rootScope = $injector.get('$rootScope');
+		$compile = $injector.get('$compile');
+		$timeout = $injector.get('$timeout');
+	}));
+
+	it('should change the text of the element to "a few seconds ago" when given current time', function () {
+		$rootScope.testDate = new Date();
+		var element = angular.element('<span am-time-ago="testDate"></span>');
+		element = $compile(element)($rootScope);
+		$rootScope.$digest();
+		expect(element.text()).toBe('a few seconds ago');
+	});
+
+	it('should change the text of the div to "3 minutes ago" when given a date 3 minutes ago', function () {
+		$rootScope.testDate = new Date(new Date().getTime() - 3 * 60 * 1000);
+		var element = angular.element('<div am-time-ago="testDate"></div>');
+		element = $compile(element)($rootScope);
+		$rootScope.$digest();
+		expect(element.text()).toBe('3 minutes ago');
+	});
+
+	it('should change the text of the div to "2 hours ago" when given a date 2 hours ago', function () {
+		$rootScope.testDate = new Date(new Date().getTime() - 2 * 60 * 60 * 1000);
+		var element = angular.element('<div am-time-ago="testDate"></div>');
+		element = $compile(element)($rootScope);
+		$rootScope.$digest();
+		expect(element.text()).toBe('2 hours ago');
+	});
+
+	it('should change the text of the div to "one year ago" when given a date one year ago', function () {
+		var today = new Date();
+		$rootScope.testDate = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+		var element = angular.element('<div am-time-ago="testDate"></div>');
+		element = $compile(element)($rootScope);
+		$rootScope.$digest();
+		expect(element.text()).toBe('a year ago');
+	});
+
+	it('should parse correctly numeric dates as milliseconds since the epoch', function () {
+		$rootScope.testDate = new Date().getTime();
+		var element = angular.element('<div am-time-ago="testDate"></div>');
+		element = $compile(element)($rootScope);
+		$rootScope.$digest();
+		expect(element.text()).toBe('a few seconds ago');
+	});
+
+	it('should update the value if date changes on scope', function () {
+		var today = new Date();
+		$rootScope.testDate = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate()).getTime();
+		var element = angular.element('<div am-time-ago="testDate"></div>');
+		element = $compile(element)($rootScope);
+		$rootScope.$digest();
+		expect(element.text()).toBe('a year ago');
+		$rootScope.testDate = new Date();
+		$rootScope.$digest();
+		expect(element.text()).toBe('a few seconds ago');
+	});
+
+	it('should update the span text as time passes', function () {
+		$rootScope.testDate = new Date(new Date().getTime() - 44000);
+		var element = angular.element('<div am-time-ago="testDate"></div>');
+		element = $compile(element)($rootScope);
+		$rootScope.$digest();
+		expect(element.text()).toBe('a few seconds ago');
+
+		waitsFor(function () {
+			return (new Date().getTime() - $rootScope.testDate.getTime()) > 45000;
+		}, '$rootScope.date is more than 45 seconds old', 1500);
+
+		runs(function () {
+			$timeout.flush();
+			$rootScope.$digest();
+			expect(element.text()).toBe('a minute ago');
+		});
+	});
 });
